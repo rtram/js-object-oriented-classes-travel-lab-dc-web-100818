01181683edda7ac871ced44831c044f2c4fe4ad1
Robin Tram <robin.tram@gmail.com>
2018-11-27 02:55:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return (year-this.startDate.getFullYear());
+  }
+}
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled() {
+    let width = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    let vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return width+vertical
+  }
+  
+  estimatedTime(boolean) {
+    if (boolean === true) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+
+  }
+}
